f9f155fa1209f2554e2ed929b1b0597885ae9279
Charmingdc <charmingdc002@gmail.com>
2024-10-16 22:54:17 +0100

    update

--- src/scripts/controller/homeController.js
+++ src/scripts/controller/homeController.js
@@ -5,7 +5,36 @@ import { getElement } from '../view/getElement.js';
 import { copyText } from '../view/copyText.js';
 import { speakText } from '../view/tts.js';
 
-// getting elements 
+// selecting elements 
+const introScreen = getElement('intro-screen', 'id');
 const dailyAffirmText = getElement('daily-affirm-text', 'id');
 const copyAffirmBtn = getElement('copy-affirm-btn', 'id');
 const speakAffirmBtn = getElement('speak-affirm-btn', 'id');
+const showRelaxTracks = getElement('show-relax-tracks', 'id');
+
+
+
+
+
+// function to hide intro screen after 2 secs
+window.onload = () => {
+  setTimeout(() => {
+    introScreen.style.display = 'none';
+  }, 2000);
+};
+
+
+// event listener to copy daily affirmation
+copyAffirmBtn.addEventListener('click', () => {
+  copyText(dailyAffirmText);
+});
+
+
+// event listener to listen to daily affirmation
+speakAffirmBtn.addEventListener('click', () => {
+  speakText('daily-affirm-text');
+});
+
+showRelaxTracks.addEventListener('click', () => {
+  alert('working');
+});
--- src/scripts/view/tts.js
+++ src/scripts/view/tts.js
@@ -1,7 +1,12 @@
 'use strict';
 
-export const speakText = (text) => {
+export const speakText = (elementId) => {
+  const element = document.getElementById(elementId);
+ const text = element.textContent;
  
+ if (!element)
+   console.error(`Element not found`);
+   
  const utterance = new SpeechSynthesisUtterance(text);
  speechSynthesis.speak(utterance);
  
