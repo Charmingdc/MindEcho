0c464d9b45b77789dc6b19f88277f2b94a8c1459
Adebayo Muis <162116910+Charmingdc@users.noreply.github.com>
2024-10-24 02:18:26 +0000

    progress

--- src/scripts/controller/homeController.js
+++ src/scripts/controller/homeController.js
@@ -17,16 +17,68 @@ const speakAffirmBtn = getElement('speak-affirm-btn', 'id');
 const showRelaxTracks = getElement('show-relax-tracks', 'id');
 const tracksContainer  = getElement('tracks-container', 'id');
 const closeTrackCont = getElement('close-tracks-cont', 'id');
+const trackCats = getElement('track-cats', 'id');
 
 
 
 
 
-// function to hide intro screen after 2 secs
+
 window.onload = () => {
+
   setTimeout(() => {
     introScreen.style.display = 'none';
-  }, 1000);
+  }, 1000); // hide intro screen after two sec
+  
+
+
+  const fetchTracks = () => {
+   const path = 'https://raw.githubusercontent.com/Charmingdc/Relaxation-tracks/main/src/moods.json';
+   
+   fetchApi(`${path}`)
+   .then(data => {
+
+     const moodArray = data.moods; // set all moods to a variable
+
+     // loop through all moods
+     moodArray.forEach(mood => { 
+
+      const li = document.createElement('li');
+      const h2 = document.createElement('h2');
+  
+      h2.textContent = mood.mood;
+      li.appendChild(h2);
+      trackCats.appendChild(li);
+      
+
+      // loop through all moods tracks
+      mood.tracks.forEach(track => {
+        const playBtn = `<i class="fa-solid fa-circle-play">`; // create a variable for play icon
+
+        const trackDp = document.createElement('div'); // create trackDp
+        trackDp.innerHTML = playBtn; // set Playbtn as trackDp innerHTML 
+
+        const trackName = document.createElement('h3');
+        trackName.textContent = track.name; // created a h3 header and set track name as its text
+
+        const trackInfo = document.createElement('div');
+        trackInfo.appendChild(trackName); // created a div for track Infos and append trackName to it
+
+        const trackBox = document.createElement('div');
+        trackBox.appendChild(trackDp);
+        trackBox.appendChild(trackInfo);
+
+        const trackBoxWrap = document.createElement('div');
+        trackBoxWrap.classList.add('tracks-box-wrapper');
+        trackBoxWrap.appendChild(trackBox);
+        
+        li.appendChild(trackBoxWrap)
+      });
+
+     });
+   });
+  }
+  fetchTracks(); // function to fetch and display tracks by categories 
 };
 
 
