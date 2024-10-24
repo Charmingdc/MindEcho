d176b347a9438996ccb30803c5ed63e6a1cbf7f0
Adebayo Muis <162116910+Charmingdc@users.noreply.github.com>
2024-10-24 04:02:00 +0100

    Update homeController.js

--- src/scripts/controller/homeController.js
+++ src/scripts/controller/homeController.js
@@ -50,38 +50,6 @@ window.onload = () => {
       li.appendChild(h2);
       trackCats.appendChild(li);
       
-
-      // loop through all moods tracks
-      mood.tracks.forEach(track => {
-        const playBtn = `<i class="fa-solid fa-circle-play">`; // create a variable for play icon
-
-        const trackDp = document.createElement('div'); // create trackDp
-        trackDp.innerHTML = playBtn; // set Playbtn as trackDp innerHTML 
-
-        const trackName = document.createElement('h3');
-        trackName.textContent = track.name; // created a h3 header and set track name as its text
-
-        const trackMin = document.createElement('p');
-        const audio = new Audio(track.media_url);
-        audio.onloadedmetadata = () => {
-          trackMin.textContent = (audio.duration / 60).toFixed(2);
-        }
-
-        const trackInfo = document.createElement('div');
-        trackInfo.appendChild(trackName); // created a div for track Infos and append trackName to it
-        trackInfo.appendChild(trackMin);
-
-        const trackBox = document.createElement('div');
-        trackBox.appendChild(trackDp);
-        trackBox.appendChild(trackInfo);
-
-        const trackBoxWrap = document.createElement('div');
-        trackBoxWrap.classList.add('tracks-box-wrapper');
-        trackBoxWrap.appendChild(trackBox);
-        
-        li.appendChild(trackBoxWrap);
-      });
-
      });
    });
   }
