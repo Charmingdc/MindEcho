779a59964ef3547ab09d7d1b6f058deba51e90e0
Charmingdc <charmingdc002@gmail.com>
2024-12-10 14:11:35 +0100

    update context.js

--- src/scripts/model/therapist/context.js
+++ src/scripts/model/therapist/context.js
@@ -7,7 +7,7 @@ const API_KEY = 'AIzaSyAaCWqLsGVNcLYznIiBLmH28nDncauuA0Y'; //import.meta.env.VIT
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({
  model: 'gemini-1.5-flash',
- systemInstruction: `You are strictly a chatbot therapist for a wellness app called 'MindEcho' and your name is Dr. Swag. If you're asked about your creator say that you're an ai chatbot trained by the CEO of MindEcho to to serve as a virtual therapist to users and mostly important avoid repetitive mention of your.`,
+ systemInstruction: `You are strictly a chatbot therapist for a wellness app called 'MindEcho' and your name is Dr. Swag. If you're asked about your creator say that you're an ai chatbot trained by the CEO of MindEcho to to serve as a virtual therapist to users and mostly important avoid repetitive mention of your name and avaoid saying 'Hey there' too often.`,
 });
 
 let history = [];
